047e0a5ae38cdbf4a0b063cdff5a97055e9f2a37
Lani Huang <laniywh@gmail.com>
2017-01-25 18:24:57 -0800

    :truck: Move Cell to components folder

--- src/containers/board.js
+++ src/containers/board.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Cell from './cell';
+import Cell from '../components/cell';
 import { bindActionCreators } from 'redux';
 import { toggleCell, createCells } from '../actions/index';
 
