f2dda6f3ab406f8eeacff77ff34146011ff287e9
Lani Huang <laniywh@gmail.com>
2017-01-27 16:00:38 -0800

    Modify config for heroku deployment

---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: '/public/',
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
